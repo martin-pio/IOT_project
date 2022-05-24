8c663ceb0c08d470bdb6e5c17fee2ed58a143afa
martin <spambam1203@gmail.com>
2022-05-24 12:59:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+
+      <div class="gear">
+        <i class="fa fa-cog" aria-hidden="true"></i>
+      </div>
+      <div class="container">
+        <div class="wrapper">
+          <div class="message">
+            <h1>Nwarehouse Pvt.Ltd</h1>
+
+            <h2>Website is Under Construction</h2>
+            <p>
+              We sincerely apologize for the inconvenience. Our site is
+              currently under Construction, but will return shortly. Thank you
+              for your patience.
+            </p>
+
+            <div class="contact-box">
+              <h4>You can still contact us using the following methods:</h4>
+              <b>Phone: +91 9360291185</b> <br />
+              <b>Email: nwarehouse2020@gmail.com</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="gear">
+        <i class="fa fa-cog" aria-hidden="true"></i>
+      </div>
+    </>
+  );
+};
+
+export default App;
